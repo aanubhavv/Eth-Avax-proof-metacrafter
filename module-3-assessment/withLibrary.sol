1033018ada469d741b1a759ecd8a7b4d37873b51
aanubhavv <117589083+aanubhavv@users.noreply.github.com>
2024-05-31 22:55:04 +0530

    Update withLibrary.sol

--- module-3-assessment/withLibrary.sol
+++ module-3-assessment/withLibrary.sol
@@ -7,16 +7,24 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+//OpenZeppelin is an open-source framework that provides us with functionality to deploy tokens on blockchain
 
-    // Mint function to allow the owner to mint new tokens
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("Warp", "WRP") {}
+
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 
-    // Burn function to allow any user to burn their tokens
+    function transferTokens(address to, uint256 amount) external {
+        require(to != address(0), "provide correct address");
+        require(balanceOf(msg.sender) >= amount, "transfer amount exceeds balance");
+        
+        _transfer(msg.sender, to, amount);
+    }
+
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
     }
+
 }
